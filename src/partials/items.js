e5e2a668123290577e99f1a3b595a3cced534a95
StasGill <stasgill@gmail.com>
2019-11-06 11:15:47 +0200

    SG

---
+++ src/partials/items.js
@@ -0,0 +1,39 @@
+const items = [
+  {
+    name: "Airmax",
+    price : 180,
+    descFull : "The bold silhouette of Nike Air lifts the Nike Air Max 270
+           React to new heights, while the Nike React foam midsole delivers exceptional
+           cushioning. Imagine all-day comfort with unstoppable style.";
+    desc  :     "The bold silhouette of Nike Air lifts the Nike Air Max 270";
+    gender : "male";
+    src : "";
+  }
+  {
+    name: "Airmax";
+    price : 180 "$" ;
+    descFull : "The bold silhouette of Nike Air lifts the Nike Air Max 270
+           React to new heights, while the Nike React foam midsole delivers exceptional
+           cushioning. Imagine all-day comfort with unstoppable style.";
+    desc  :     "The bold silhouette of Nike Air lifts the Nike Air Max 270";
+    gender : "male";
+  }
+  {
+    name: "Airmax";
+    price : 180 "$" ;
+    descFull : "The bold silhouette of Nike Air lifts the Nike Air Max 270
+               React to new heights, while the Nike React foam midsole delivers exceptional
+               cushioning. Imagine all-day comfort with unstoppable style.";
+    desc  :     "The bold silhouette of Nike Air lifts the Nike Air Max 270";
+    gender : "male";
+  }
+  {
+    name: "Airmax";
+    price : 180 "$" ;
+    descFull : "The bold silhouette of Nike Air lifts the Nike Air Max 270
+           React to new heights, while the Nike React foam midsole delivers exceptional
+           cushioning. Imagine all-day comfort with unstoppable style.";
+    desc  :     "The bold silhouette of Nike Air lifts the Nike Air Max 270";
+    gender : "male";
+  }
+]
