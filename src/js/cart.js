c5a4bddbf6bff55b126f725a7e6ab4aeccec74bd
PoisonUA <poison_mailbox@ukr.net>
2019-11-07 11:54:50 +0200

    some parasha

--- src/js/cart.js
+++ src/js/cart.js
@@ -2,13 +2,12 @@ const cart = document.querySelector('.cart');
 const cartTag = document.querySelector('.cart__tag');
 const cartTagImage = document.querySelector('.cart__tagimage');
 
-console.log(cartTagImage);
+
+
 const cartOpenClose = (event) => {
-    console.log(event.target);
-    console.log(event.currentTarget);
+
     if (event.target === cartTag || event.target === cartTagImage) {
         cartTag.classList.toggle('cart__tag--isopen');
     }
 }
 cartTag.addEventListener('click', cartOpenClose);
-console.log(cart);
