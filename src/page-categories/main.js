0b470c1b87b3a870167fee241890bb041f6affb7
Viktor Mryga <dontworryweareinsafe@gmail.com>
2019-11-07 13:28:04 +0200

    navigation without bg

--- src/page-categories/main.js
+++ src/page-categories/main.js
@@ -1,5 +1,7 @@
 import '../scss/main.scss';
 import './page.scss';
+import '../js/index';
+import '../js/cart.js';
 
 document.addEventListener('DOMContentLoaded', () => {
   console.log('DOMContentLoaded', 'page-contacts');
