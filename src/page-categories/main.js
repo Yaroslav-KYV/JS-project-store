9b1edb98ebd3734e12699812c4263f07b442eb77
RBCHK <artem.rybachuk@gmail.com>
2019-11-07 11:15:54 +0200

    add visa

--- src/page-categories/main.js
+++ src/page-categories/main.js
@@ -4,3 +4,11 @@ import './page.scss';
 document.addEventListener('DOMContentLoaded', () => {
   console.log('DOMContentLoaded', 'page-contacts');
 });
+
+// Слайдер брендов
+$('.autoplay').slick({
+  slidesToShow: 3,
+  slidesToScroll: 1,
+  autoplay: true,
+  autoplaySpeed: 2000,
+});
