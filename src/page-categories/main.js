3f451442e9352e1f3b218e374ffa18ba82d0b2ba
Yaroslav <kovshun.yv@gmail.com>
2019-11-07 17:43:52 +0200

    clear unused parts from webpack, add navigation bar to pages: about, admin-products

--- src/page-categories/main.js
+++ src/page-categories/main.js
@@ -2,8 +2,4 @@ import '../scss/main.scss';
 import './page.scss';
 import '../js/brandSlider.js'
 
-document.addEventListener('DOMContentLoaded', () => {
-  console.log('DOMContentLoaded', 'page-contacts');
-});
-
 
