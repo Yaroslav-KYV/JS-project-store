ffed2659e5aa0a58bc1d24e857d3833480993a6c
StasGill <stasgill@gmail.com>
2019-11-08 12:15:34 +0200

    123

--- src/page-admin-products/main.js
+++ src/page-admin-products/main.js
@@ -1,2 +1,56 @@
 import '../scss/main.scss';
 import './page.scss';
+
+
+
+
+const addItemForm = document.querySelector('.AddNewItem');
+const addItemButton = document.querySelector('.addItemButton');
+
+console.log(addItemButton);
+
+
+const newItem = [];
+
+addItemForm.addEventListener('submit', takeData)
+
+function takeData(e){
+  e.preventDefault();
+  const { name, price, image,  } = e.target.elements;
+  const obj = {
+    image: image.files[0],
+    price: price.value,
+  };
+  console.log('obj', obj);
+
+  const formData  = new FormData();
+  Object.keys(obj).forEach(key => {
+      formData.append(key, data[key]);
+  })
+}
+
+
+
+// const postToAdd = {
+//   author: 'Mango',
+//   body: 'CRUD is awesome',
+// };
+
+
+
+
+// fetch('https://jsonplaceholder.typicode.com/posts', {
+//   method: 'POST',
+//   body: JSON.stringify(postToAdd),
+//   headers: {
+//     'Content-Type': 'application/json; charset=UTF-8',
+//   },
+// })
+//   .then(response => response.json())
+//   .then(post => console.log(post))
+//   .catch(error => console.log(error));
+
+
+
+
+
