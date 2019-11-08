6a862a22ee0c4c67ad33cae930354072e05f6597
StasGill <stasgill@gmail.com>
2019-11-08 12:50:32 +0200

    dsd

--- src/page-admin-products/main.js
+++ src/page-admin-products/main.js
@@ -35,24 +35,6 @@ function takeData(e){
 
 
 
-// const postToAdd = {
-//   author: 'Mango',
-//   body: 'CRUD is awesome',
-// };
-
-
-
-
-// fetch('https://jsonplaceholder.typicode.com/posts', {
-//   method: 'POST',
-//   body: JSON.stringify(postToAdd),
-//   headers: {
-//     'Content-Type': 'application/json; charset=UTF-8',
-//   },
-// })
-//   .then(response => response.json())
-//   .then(post => console.log(post))
-//   .catch(error => console.log(error));
 
 
 
