5922beacdfe9c4060a1979a871fa7ee237940654
StasGill <stasgill@gmail.com>
2019-11-08 16:52:16 +0200

    ert

--- src/js/utils/api.js
+++ src/js/utils/api.js
@@ -161,3 +161,27 @@ export const getShopData = () => {
   return Promise.resolve(shopData)
 
 }
+
+
+export const signUp = (userData) => {
+  return fetch('http://localhost:3000/api/auth/signin', {
+    method: 'POST',
+    headers: {
+        'content-type': 'application/json',
+    },
+      body: JSON.stringify(userData)
+  }).then(res => res.json());
+}
+
+export const newItemFetch = (formData, token) => {
+  return fetch('http://localhost:3000/api/products', {
+    method: 'POST',
+    headers: {
+        'Authorization': `Bearer ${token}`,
+    },
+    body: formData
+  }).then(res => res.json());
+
+}
+
+
--- src/page-admin-products/main.js
+++ src/page-admin-products/main.js
@@ -1,14 +1,49 @@
 import '../scss/main.scss';
 import './page.scss';
 import './login.scss';
+import { signUp } from '../js/utils/api';
+import { newItemFetch } from '../js/utils/api';
+import { shopData } from '../js/utils/api';
+import store from '../js/store';
 
 
 
 
 const addItemForm = document.querySelector('.AddNewItem');
-const addItemButton = document.querySelector('.addItemButton');
+const loginFrom = document.querySelector('.login');
+const select = document.querySelector('.select');
+const loginButton = document.querySelector('.loginButton');
+const loginContainer = document.querySelector('.adminContainer');
+
+const addItemWrapper = document.querySelector('.addItemContainer');
 
 
+
+loginFrom.addEventListener('submit',e => {
+  e.preventDefault();
+  const {email, password} = e.target.elements;
+  const loginCheck = {
+    email: email.value,
+    password: password.value,
+  }
+  console.log('login :', loginCheck);
+  const formDataSign  = new FormData();
+
+  Object.keys(loginCheck).forEach(key => {
+      formDataSign.append(key, loginCheck[key]);
+  })
+  signUp(loginCheck).then(resData => {
+    console.log('resData', resData);
+    if(resData.success && resData.token) {
+      store.admin = {
+        token: resData.token,
+        auth: true,
+      }
+      addItemWrapper.style.display = "block";
+      loginContainer.style.display = "none";
+    }
+  })
+})
 
 
 const newItem = [];
@@ -17,13 +52,14 @@ addItemForm.addEventListener('submit', takeData)
 
 function takeData(e){
   e.preventDefault();
-  const { image, price, description, fullDescription, name } = e.target.elements;
+  const { image, price, description, fullDescription, name , category} = e.target.elements;
   const obj = {
     image: image.files[0],
     price: price.value,
     description: description.value,
     fullDescription: fullDescription.value,
     name: name.value,
+    category: select.options[select.selectedIndex].value,
  };
   console.log('obj', obj);
 
@@ -31,9 +67,16 @@ function takeData(e){
   Object.keys(obj).forEach(key => {
       formData.append(key, obj[key]);
   })
+  if(store.admin.token) {
+    newItemFetch(formData, store.admin.token).then(resData => {
+      console.log('resData', resData);
+      store.shopData = resData.products;
+      console.log('store :', store);
+    })
+
+  }
+
 }
-
-
 
 
 
