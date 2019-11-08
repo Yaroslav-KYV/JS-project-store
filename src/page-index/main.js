ecd12ca5a8c56546e50f0ab5b9aad2cf9595300b
Viktor Mryga <dontworryweareinsafe@gmail.com>
2019-11-08 11:20:46 +0200

    merge

--- src/page-index/main.js
+++ src/page-index/main.js
@@ -2,10 +2,14 @@ import '../js/index';
 import '../scss/main.scss';
 import './page.scss';
 import '../js/cart.js';
+// import 'slick-carousel';
+// import 'slick-carousel/slick/slick.scss';
 
 document.addEventListener('DOMContentLoaded', () => {
   console.log('DOMContentLoaded', 'page-index')
 })
+
+$('.slider_offer__list').slick();
 
 class Foo {
   constructor () {
