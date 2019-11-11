645597e0961515f752599d7793ab911dd1355c1b
RBCHK <artem.rybachuk@gmail.com>
2019-11-11 17:44:38 +0200

    for merge with dev

--- src/js/signupModal.js
+++ src/js/signupModal.js
@@ -1,9 +1,1 @@
-const knopka = document.querySelector('.knopka');
-console.log(knopka)
-const modalWrapper = document.querySelector('.signup-modal__wrapper');
-console.log(modalWrapper)
 
-knopka.addEventListener('click', function (e) {
-  e.preventDefault();
-  modalWrapper.classList.add('active');
-});
